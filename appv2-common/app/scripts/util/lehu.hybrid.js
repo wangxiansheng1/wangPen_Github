b031cf06ec52e0c34ca5e8cd641aaa33a61cf912
季严亮 <jiyanliang@jiyanliangdeMacBook-Pro.local>
2016-09-13 09:43:48 +0800

    修改bug

--- appv2-common/app/scripts/util/lehu.hybrid.js
+++ appv2-common/app/scripts/util/lehu.hybrid.js
@@ -11,6 +11,23 @@ define(
   function($, can, _, md5, store, LHUtil) {
     'use strict';
 
+    var setupWebViewJavascriptBridge = function(callback) {
+      if (window.WebViewJavascriptBridge) {
+        return callback(WebViewJavascriptBridge);
+      }
+      if (window.WVJBCallbacks) {
+        return window.WVJBCallbacks.push(callback);
+      }
+      window.WVJBCallbacks = [callback];
+      var WVJBIframe = document.createElement('iframe');
+      WVJBIframe.style.display = 'none';
+      WVJBIframe.src = 'wvjbscheme://__BRIDGE_LOADED__';
+      document.documentElement.appendChild(WVJBIframe);
+      setTimeout(function() {
+        document.documentElement.removeChild(WVJBIframe)
+      }, 0)
+    };
+
     //获得URL地址
     var getUrl = function() {
       var url = {};
